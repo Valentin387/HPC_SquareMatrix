b617902d15a3e5a611023d25319fdc7427ccf8b2
ValentinVV42 <valentin.valencia.1@ens.etsmtl.ca>
2023-08-15 10:36:29 -0500

    I got it parametric

--- main.c
+++ main.c
@@ -1,29 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 //Valentín Valencia Valencia. UTP Pereira, Risaralda, Colombia
 
-void fillMatrix(int matrix[], int rows, int cols, int fillMethod) {
-    if (fillMethod == 1) {
-        printf("Enter the elements of the matrix:\n");
-        int i = 0;
-        int j = 0;
-        for (i = 0; i < rows; i++) {
-            for (j = 0; j < cols; j++) {
-                scanf("%d", &matrix[i * cols + j]);
-            }
-        }
-    } else if (fillMethod == 2) {
-        srand(time(NULL));
-        int i = 0;
-        int j = 0;
-        for (i = 0; i < rows; i++) {
-            for (j = 0; j < cols; j++) {
-                matrix[i * cols + j] = rand() % 101;
-            }
+void fillMatrix(int matrix[], int rows, int cols) {
+
+    srand(time(NULL));
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            matrix[i * cols + j] = rand() % 101;
         }
     }
+    
 }
 
 void multiplyMatrices(int result[], int matrix1[], int matrix2[], int rows1, int cols1, int cols2) {
@@ -56,68 +48,49 @@ void printMatrix(int *matrix, int rows, int cols) {
     }
     printf("\n");
 }
-int main() {
-    int option;
-    int rows1, cols1, rows2, cols2;
-    int fillMethod;
-    
-    while (1) {
-        printf("\n\nMatrix Multiplication Program\n");
-        printf("1) Generate new multiplication\n");
-        printf("2) End program\n");
-        printf("Enter your choice: ");
-        scanf("%d", &option);
-        
-        if (option == 1) {
-            printf("Enter the dimensions (rows and columns) of the first matrix Ex: 2 3: ");
-            if (scanf("%d %d", &rows1, &cols1) != 2) {
-                printf("Invalid input for dimensions. Please enter two numbers.\n");
-                while (getchar() != '\n');  // Clear input buffer
-                continue;
-            }
-            printf("Enter the dimensions (rows and columns) of the second matrix Ex: 2 3: ");
-            if (scanf("%d %d", &rows2, &cols2) != 2) {
-                printf("Invalid input for dimensions. Please enter two numbers.\n");
-                while (getchar() != '\n');  // Clear input buffer
-                continue;
-            }
-            
-            if (cols1 != rows2) {
-                printf("\t\t WARNING ERROR Matrix dimensions are not suitable for multiplication.\n");
-                continue;
-            }
-            
-            printf("\n\nChoose filling method:\n");
-            printf("1) Fill element by element\n");
-            printf("2) Generate random values (0 to 100)\n");
-            printf("Enter your choice: ");
-            scanf("%d", &fillMethod);
-            
-            int matrix1[rows1][cols1];
-            int matrix2[rows2][cols2];
-            int result[rows1][cols2];
-            
-            fillMatrix(&matrix1[0][0], rows1, cols1, fillMethod);
-            fillMatrix(&matrix2[0][0], rows2, cols2, fillMethod);
-            
-            multiplyMatrices(&result[0][0], &matrix1[0][0], &matrix2[0][0], rows1, cols1, cols2);
-            
-            printf("\nMatrix 1:\n");
-            printMatrix(&matrix1[0][0], rows1, cols1);
-            
-            printf("\nMatrix 2:\n");
-            printMatrix(&matrix2[0][0], rows2, cols2);
-            
-            printf("\nResulting Matrix:\n");
-            printMatrix(&result[0][0], rows1, cols2);
-        } else if (option == 2) {
-            printf("END OF LINE.\n");
-            break;
-        } else {
-            printf("Invalid option. Please enter 1 or 2.\n");
-        }
+int main(int argc, char* argv[]) {
+	if (argc != 3) {
+        printf("Usage: %s N Verbose\n", argv[0]);
+        return 1;
     }
+
+    int N = atoi(argv[1]);
+    bool verbose = atoi(argv[2]);
+
+    srand(time(NULL));
+    int rows1=N;
+	int cols1=N; 
+	int rows2=N;
+	int cols2=N;
     
+    int matrix1[rows1][cols1];
+    int matrix2[rows2][cols2];
+    int result[rows1][cols2];
+    
+    fillMatrix(&matrix1[0][0], rows1, cols1);
+    fillMatrix(&matrix2[0][0], rows2, cols2);
+    
+	clock_t start_time = clock();
+    
+    multiplyMatrices(&result[0][0], &matrix1[0][0], &matrix2[0][0], rows1, cols1, cols2);
+    
+    clock_t end_time = clock();
+    double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    
+    printf("Matrix multiplication took %.6f seconds.\n", elapsed_time);
+
+    
+    if (verbose){
+    	printf("\nMatrix 1:\n");
+	    printMatrix(&matrix1[0][0], rows1, cols1);
+	    
+	    printf("\nMatrix 2:\n");
+	    printMatrix(&matrix2[0][0], rows2, cols2);
+	    
+	    printf("\nResulting Matrix:\n");
+	    printMatrix(&result[0][0], rows1, cols2);
+	}
+
     system("PAUSE");
     return 0;
 }
