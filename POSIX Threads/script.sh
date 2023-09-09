b3725d472f474a73f2bbd1e44c14157f2694b369
Valentin387 <valentin.valencia@utp.edu.co>
2023-09-09 18:09:02 -0500

    added file Naming conventions

--- POSIX Threads/script.sh
+++ POSIX Threads/script.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-rm times2.doc
+rm times2Threads.doc
 for j in {1..10}
 do
 for i in 100 500 800 900 1000 2000
 do
-./ExeValentin $i 0 >> times2.doc
+./ExeValentin $i 0 >> times2Threads.doc
 done
 done
