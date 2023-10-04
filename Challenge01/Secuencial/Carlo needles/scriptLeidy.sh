c8dc664235e1213925588d3ba5ca564504b38d63
Leidy Martinez <leidy.martinez@utp.edu.co>
2023-10-04 15:25:56 -0500

    Exes con O3

--- Challenge01/Secuencial/Carlo needles/scriptLeidy.sh
+++ Challenge01/Secuencial/Carlo needles/scriptLeidy.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-rm timesLeidy.doc
+rm timesLeidyO3.doc
 for j in {1..10}
 do
 for i in 1000 20000 3000000 50000000 80000000
 do
-./ExeLeidy $i >> timesLeidy.doc
+./ExeLeidyO3 $i >> timesLeidyO3.doc
 done
 done
