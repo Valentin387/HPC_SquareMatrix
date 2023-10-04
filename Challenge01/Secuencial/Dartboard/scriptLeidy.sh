43dc97062c346cd36daf44e82c720c09565f731b
Leidy Martinez <leidy.martinez@utp.edu.co>
2023-10-04 14:43:37 -0500

    Times Leidy

--- Challenge01/Secuencial/Dartboard/scriptLeidy.sh
+++ Challenge01/Secuencial/Dartboard/scriptLeidy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-rm timesLeidy.doc
+rm timesValentin.doc
 for j in {1..10}
 do
 for i in 1000 20000 3000000 50000000 80000000
--- Challenge01/Threads/Carlo needles/scriptLeidy.sh
+++ Challenge01/Threads/Carlo needles/scriptLeidy.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-rm timesLeidy.doc
+rm times16Threads.doc
 for j in {1..10}
 do
 for i in 1000 20000 3000000 50000000 80000000
 do
-./ExeLeidy $i >> timesLeidy.doc
+./ExeLeidy $i >> times16Threads.doc
 done
 done
