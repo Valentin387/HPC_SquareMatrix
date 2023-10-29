fd6e7eac737757407acc8ff41f4f532ab05bd486
ValentinVV42 <valentin.valencia.1@ens.etsmtl.ca>
2023-10-28 19:05:13 -0500

    matrices times valentin done

--- openMP/matrices/script.sh
+++ openMP/matrices/script.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-rm times2Valentin.doc
+rm times16Valentin.doc
 for j in {1..10}
 do
 for i in 100 500 800 900 1000 2000
 do
-./ExeValentin $i 0 2 >> times2Valentin.doc
+./ExeValentin $i 0 16 >> times16Valentin.doc
 done
 done
